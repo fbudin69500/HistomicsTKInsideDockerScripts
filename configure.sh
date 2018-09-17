d657b0034682959d3fc075dca994936f43eb406a
Francois Budin <francois.budin@kitware.com>
2018-09-17 13:25:49 -0400

    Simplify scripts now that HistomicsTK supports testing inside docker container.

--- configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker exec -it histomicstk_histomicstk bash -c "
-  cd /opt/histomicstk/girder && \
-  npm install && \
-  sudo npm install -g npx && \
-  sudo girder-install plugin -s /opt/histomicstk/HistomicsTK"
